7b3a65616030fc0d2a798ab25341b0d7bf349e57
kmakrutin <k.makrutin@gmail.com>
2019-07-15 17:31:14 +0300

    Ch 9 - OAuth2 with @EnableOAuth2Sso auto configuration

---
+++ Chapter09/springboot_n_oauth2_tutorial/src/main/java/com/kmakrutin/springboot_n_oauth2_tutorial/SpringbootNOauth2TutorialApplication.java
@@ -0,0 +1,15 @@
+package com.kmakrutin.springboot_n_oauth2_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNOauth2TutorialApplication
+{
+
+  public static void main( String[] args )
+  {
+    SpringApplication.run( SpringbootNOauth2TutorialApplication.class, args );
+  }
+
+}
---
+++ Chapter09/springboot_n_oauth2_tutorial/src/test/java/com/kmakrutin/springboot_n_oauth2_tutorial/SpringbootNOauth2TutorialApplicationTests.java
@@ -0,0 +1,18 @@
+package com.kmakrutin.springboot_n_oauth2_tutorial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith( SpringRunner.class )
+@SpringBootTest
+public class SpringbootNOauth2TutorialApplicationTests
+{
+
+  @Test
+  public void contextLoads()
+  {
+  }
+
+}
