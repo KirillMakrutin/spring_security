ad73591f76bab115b4e64f16fcd5c3cc5e76e6bd
kmakrutin <k.makrutin@gmail.com>
2019-08-05 15:46:51 +0300

    Chapter 10-00 Start

---
+++ Chapter10/chapter-10-00/src/main/java/com/kmakrutin/calendar/authentication/RedirectToHttpsConfig.java
@@ -0,0 +1,52 @@
+package com.kmakrutin.calendar.authentication;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Not necessary, but in some cases it might be a good idea to make your application accessible over HTTP too, but redirect
+ * all traffic to HTTPS. To achieve this we’ll need to add a second Tomcat connector, but currently it is not possible
+ * to configure two connector in the application.properties like mentioned before.
+ * Because of this we’ll add the HTTP connector programmatically and make sure it redirects all traffic to our HTTPS connector.
+ *
+ * For this we will need to add the TomcatEmbeddedServletContainerFactory bean
+ */
+@Configuration
+public class RedirectToHttpsConfig
+{
+  @Bean
+  public ServletWebServerFactory servletContainer()
+  {
+    TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory()
+    {
+      @Override
+      protected void postProcessContext( Context context )
+      {
+        SecurityConstraint securityConstraint = new SecurityConstraint();
+        securityConstraint.setUserConstraint( "CONFIDENTIAL" );
+        SecurityCollection collection = new SecurityCollection();
+        collection.addPattern( "/*" );
+        securityConstraint.addCollection( collection );
+        context.addConstraint( securityConstraint );
+      }
+    };
+    tomcat.addAdditionalTomcatConnectors( redirectConnector() );
+    return tomcat;
+  }
+
+  private Connector redirectConnector()
+  {
+    Connector connector = new Connector( TomcatServletWebServerFactory.DEFAULT_PROTOCOL );
+    connector.setScheme( "http" );
+    connector.setPort( 8080 );
+    connector.setSecure( false );
+    connector.setRedirectPort( 8443 );
+    return connector;
+  }
+}
---
+++ Chapter10/chapter-10-00/src/main/java/com/kmakrutin/calendar/web/controllers/SignupController.java
@@ -0,0 +1,57 @@
+package com.kmakrutin.calendar.web.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.kmakrutin.calendar.domain.CalendarUser;
+import com.kmakrutin.calendar.services.CalendarService;
+import com.kmakrutin.calendar.services.UserContext;
+import com.kmakrutin.calendar.web.dto.SignupForm;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class SignupController
+{
+  private final CalendarService calendarService;
+  private final UserContext userContext;
+
+  @GetMapping( "/signup/form" )
+  public String signupForm( @ModelAttribute SignupForm signupForm )
+  {
+    return "signup/form";
+  }
+
+  @PostMapping( "/signup/new" )
+  public String signup( @ModelAttribute( "signupForm" ) @Valid SignupForm signupForm, BindingResult result, RedirectAttributes redirectAttributes )
+  {
+    if ( result.hasErrors() )
+    {
+      return "signup/form";
+    }
+
+    String email = signupForm.getEmail().trim();
+    if ( calendarService.findUserByEmail( email ) != null )
+    {
+      result.rejectValue( "email", "errors.signup.email", "Email address is already in use." );
+      return "signup/form";
+    }
+
+    CalendarUser calendarUser = new CalendarUser();
+    calendarUser.setEmail( email );
+    calendarUser.setFirstName( signupForm.getFirstName() );
+    calendarUser.setLastName( signupForm.getLastName() );
+    calendarUser.setPassword( signupForm.getPassword() );
+
+    userContext.setCurrentUser( calendarService.createUser( calendarUser ) );
+
+    redirectAttributes.addFlashAttribute( "message", "Success" );
+    return "redirect:/";
+  }
+}
